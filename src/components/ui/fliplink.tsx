f294f6f7205b0c4a39b7b9786dc9e27373dc53e9
beMimg <bernardommguerreiro@gmail.com>
2024-07-04 12:15:13 +0200

    Create fliplink

---
+++ src/components/ui/fliplink.tsx
@@ -0,0 +1,67 @@
+import { easeInOut, motion } from "framer-motion";
+import { ArrowUpRight } from "lucide-react";
+
+const DURATION = 0.25;
+const STAGGER = 0.025;
+
+const FlipLink = ({ children, href }: { children: any; href: string }) => {
+  return (
+    <motion.a
+      initial="initial"
+      whileHover="hovered"
+      className="relative block overflow-hidden whitespace-nowrap text-3xl uppercase md:text-4xl "
+      href={href}
+    >
+      <div>
+        {children.split("").map((l: any, i: any) => {
+          return (
+            <motion.span
+              // Why i+1 conditional rendering style? If the Icon is font-medium > it will have by default a blue background.
+              // The ArrowUpRight will be always the last character of the string so, font-normal.
+              className={`inline-block ${
+                i + 1 === children.length ? "" : "font-bold"
+              } `}
+              variants={{
+                initial: { y: 0 },
+                hovered: { y: "-100%" },
+              }}
+              transition={{
+                duration: DURATION,
+                ease: "easeInOut",
+                delay: STAGGER * i,
+              }}
+              key={i}
+            >
+              {l}
+            </motion.span>
+          );
+        })}
+      </div>
+      <div className="absolute inset-0">
+        {children.split("").map((l: any, i: any) => {
+          return (
+            <motion.span
+              className={`inline-block ${
+                i + 1 === children.length ? "" : "font-bold"
+              } `}
+              variants={{
+                initial: { y: "100%" },
+                hovered: { y: 0 },
+              }}
+              transition={{
+                duration: DURATION,
+                ease: "easeInOut",
+                delay: STAGGER * i,
+              }}
+              key={i}
+            >
+              {l}
+            </motion.span>
+          );
+        })}
+      </div>
+    </motion.a>
+  );
+};
+
+export default FlipLink;
--- src/pages/unauthenticated/Home.tsx
+++ src/pages/unauthenticated/Home.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import FlipLink from "../../components/ui/fliplink";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="flex flex-1 flex-col justify-center gap-8 items-center">
+      <h1 className="text-center">
+        When keyboards can be used not only for coding
+      </h1>
+      <p className=" p-lead text-center ">
+        I like writting shi### code, even more shi#### blogs
+      </p>
+      <FlipLink href="/posts">POSTS ↗</FlipLink>
+    </div>
+  );
 };
 
 export default Home;
