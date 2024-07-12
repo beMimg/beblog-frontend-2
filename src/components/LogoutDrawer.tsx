a7ad2d644bba659c7f278960103982a6deea0075
beMimg <bernardommguerreiro@gmail.com>
2024-07-12 16:39:41 +0200

    validateDOMNesting(...): <button> in LogoutDrawer

--- src/components/LogoutDrawer.tsx
+++ src/components/LogoutDrawer.tsx
@@ -23,10 +23,8 @@ const LogoutDrawer = () => {
         </DrawerHeader>
         <DrawerFooter>
           <Button className="px-10">Logout</Button>
-          <DrawerClose>
-            <Button variant="outline" className="px-10 mt-2">
-              Cancel
-            </Button>
+          <DrawerClose className="px-10 mt-2 h-10  py-2 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground">
+            Cancel
           </DrawerClose>
         </DrawerFooter>
       </DrawerContent>
