47485418e4631a464319a757e242930885fc7e5d
beMimg <bernardommguerreiro@gmail.com>
2024-07-12 11:11:32 +0200

    Add ModeToggle variants

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -9,13 +9,25 @@ import {
 } from "./ui/dropdown-menu";
 import { useTheme } from "../theme/ThemeProvider";
 
-export function ModeToggle() {
+export function ModeToggle({
+  variant,
+}: {
+  variant?:
+    | "link"
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | null
+    | undefined;
+}) {
   const { setTheme } = useTheme();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon">
+        <Button variant={variant} size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
