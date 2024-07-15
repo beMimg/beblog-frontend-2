406a9ad48949025dd188610df968850daab48dd1
beMimg <bernardommguerreiro@gmail.com>
2024-07-15 14:07:28 +0200

    PostContent handled loading and errors

--- src/components/PostContent.tsx
+++ src/components/PostContent.tsx
@@ -2,16 +2,43 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "../api/axios";
 import { Avatar, AvatarFallback, AvatarImage } from "../components/ui/avatar";
 import moment from "moment";
+import { Skeleton } from "./ui/skeleton";
 
 const PostContent = ({ id }: { id: string | undefined }) => {
-  const { data: post, isLoading } = useQuery({
+  const {
+    data: post,
+    isLoading,
+    isError,
+  } = useQuery({
     queryFn: async () => {
       const response = await axios.get(`/post/${id}`);
-      console.log(response.data);
       return response.data;
     },
     queryKey: ["post"],
   });
+
+  if (isLoading) {
+    return (
+      <div className="flex flex-col gap-10">
+        <Skeleton className="h-[600px] w-full object-center object-cover rounded-[--radius]" />
+        <Skeleton className="w-[40rem] h-10 rounded-full " />
+        <Skeleton className="w-[30rem] h-10 rounded-full " />
+        <Skeleton className="w-full h-5 rounded-full " />
+        <Skeleton className="w-full h-5 rounded-full " />
+        <Skeleton className="w-full h-5 rounded-full " />
+        <Skeleton className="w-full h-5 rounded-full " />
+        <Skeleton className="w-full h-5 rounded-full " />
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <p className="text-destructive">
+        Something went wrong, please try again later
+      </p>
+    );
+  }
 
   return (
     post && (
@@ -45,7 +72,7 @@ const PostContent = ({ id }: { id: string | undefined }) => {
         </div>
 
         <div
-          className="mt-10"
+          className="mt-10 pb-5 border-b border-secondary"
           dangerouslySetInnerHTML={{ __html: post.content }}
         />
       </>
